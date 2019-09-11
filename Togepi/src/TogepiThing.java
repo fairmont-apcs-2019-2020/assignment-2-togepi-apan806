2fe9dfe1d02046cdc7cb7baee30f42dffb0765d9
andrewpan <andrewpan@localhost>
2019-09-10 23:27:12 -0700

    hi

--- Togepi/src/TogepiThing.java
+++ Togepi/src/TogepiThing.java
@@ -1,14 +1,14 @@
-
 public class TogepiThing extends PokeThing {
 
-	public TogepiThing()
-	{
-		super("togepi");
-	}
-	
-	public void step()
-	{
-		move(null);
-	}
-	
+public TogepiThing()
+{
+super("togepi");
+}
+
+public void step()
+
+{
+move();
+}
+
 }
